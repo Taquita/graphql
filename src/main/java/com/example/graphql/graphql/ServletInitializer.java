3f69f0903ba26f3236d01e66a1ed7da28b622c8e
Jonathan Taquita Gomes <takita.jonathan@gmail.com>
2019-02-15 19:32:13 -0400

    Initial commit

---
+++ src/main/java/com/example/graphql/graphql/ServletInitializer.java
@@ -0,0 +1,14 @@
+package com.example.graphql.graphql;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(GraphqlApplication.class);
+    }
+
+}
+
