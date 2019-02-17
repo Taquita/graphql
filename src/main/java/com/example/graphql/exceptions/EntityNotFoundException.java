e8291b5f7c30170726abd8f53e66898a9fc0c6c5
Jonathan Taquita <takita.jonathan@gmail.com>
2019-02-17 16:22:47 -0400

    create ExceptionHandler

--- src/main/java/com/example/graphql/exceptions/EntityNotFoundException.java
+++ src/main/java/com/example/graphql/exceptions/EntityNotFoundException.java
@@ -1,38 +1,9 @@
 package com.example.graphql.exceptions;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import graphql.ErrorType;
-import graphql.GraphQLError;
-import graphql.language.SourceLocation;
-
-import java.util.List;
-
-public class EntityNotFoundException extends javax.persistence.EntityNotFoundException implements GraphQLError {
-
-    private int status;
+public class EntityNotFoundException extends ExceptionHandler {
 
     public EntityNotFoundException(String message, int status) {
-        super(message);
-        this.status = status;
+        super(message, status);
     }
 
-    public int getStatus() {
-        return status;
-    }
-
-    @Override
-    public List<SourceLocation> getLocations() {
-        return null;
-    }
-
-    @Override
-    public ErrorType getErrorType() {
-        return ErrorType.ValidationError;
-    }
-
-    @Override
-    @JsonIgnore
-    public StackTraceElement[] getStackTrace() {
-        return super.getStackTrace();
-    }
 }
---
+++ src/main/java/com/example/graphql/exceptions/ExceptionHandler.java
@@ -0,0 +1,70 @@
+package com.example.graphql.exceptions;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import graphql.ErrorType;
+import graphql.GraphQLError;
+import graphql.language.SourceLocation;
+import org.springframework.http.HttpStatus;
+
+import java.util.List;
+import java.util.Map;
+
+public abstract class ExceptionHandler extends RuntimeException implements GraphQLError {
+
+    private int code;
+
+    ExceptionHandler(String message, int code) {
+        super(message);
+        this.code = code;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public String getStatus() {
+        return HttpStatus.valueOf(code).name();
+    }
+
+    @Override
+    @JsonIgnore
+    public Throwable getCause() {
+        return null;
+    }
+
+    @Override
+    @JsonIgnore
+    public String getLocalizedMessage() {
+        return this.getMessage();
+    }
+
+    @Override
+    @JsonIgnore
+    public List<SourceLocation> getLocations() {
+        return null;
+    }
+
+    @Override
+    @JsonIgnore
+    public ErrorType getErrorType() {
+        return null;
+    }
+
+    @Override
+    @JsonIgnore
+    public List<Object> getPath() {
+        return null;
+    }
+
+    @Override
+    @JsonIgnore
+    public Map<String, Object> getExtensions() {
+        return null;
+    }
+
+    @Override
+    @JsonIgnore
+    public StackTraceElement[] getStackTrace() {
+        return super.getStackTrace();
+    }
+}
